544851db030c57c8aa6d419d624ab5acebc58ce5
Andrew <samovandrej23@gmail.com>
2025-02-05 12:06:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
-API_TOKEN = 'ваш токен бота'
-GROUP_CHAT_ID = #ваш айди группы
-PROXY_URL = 'ваш прокси сервер'
+#API_TOKEN = ваш токен бота
+#GROUP_CHAT_ID = ваш айди группы
+#PROXY_URL = ваш прокси сервер
